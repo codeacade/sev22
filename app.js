f397fca3132401e8c650d1338db3977f9ef3763b
codeacade <codeacade@gmail.conm>
2022-07-16 08:24:45 +0100

    six stars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req,res) => {res.send('<h2>***** Home *****</h2><a href="/info">see page info</a>')});
-app.get('/info', (req,res) => {res.send('<h2>***** Page Info *****</h2><a href="/">back home</a>')});
+app.get('/', (req,res) => {res.send('<h2>****** Home ******</h2><a href="/info">see page info</a>')});
+app.get('/info', (req,res) => {res.send('<h2>****** Page Info ******</h2><a href="/">back home</a>')});
 app.listen(process.env.PORT || 3000);
